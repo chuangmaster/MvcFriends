af01b020b4df683084450d59516248d6c880b876
chuangmaster <chuang.master@gmail.com>
2024-01-23 09:27:23 +0800

    project init

---
+++ Models/Friend.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
+
+namespace MvcFriends.Models
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+        
+        [NotMapped]
+        public string Country { get; set; }
+    }
+}
