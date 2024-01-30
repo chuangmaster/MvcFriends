1cd0275a25f881423a150b6d844e6c05f49b461f
chuangmaster <chuang.master@gmail.com>
2024-01-31 03:37:32 +0800

    印出環境路徑、手動加入靜態路徑

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
+using Microsoft.Extensions.FileProviders;
 using MvcFriends.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+// var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ApplicationName = typeof(Program).Assembly.FullName,
+    EnvironmentName = Environments.Staging,
+    ContentRootPath = Directory.GetCurrentDirectory(),
+    // WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "StaticFileLibrary")
+});
+
+System.Console.WriteLine($"Application Name: {builder.Environment.ApplicationName}");
+System.Console.WriteLine($"Environment Name: {builder.Environment.EnvironmentName}");
+System.Console.WriteLine($"ContentRoot Name: {builder.Environment.ContentRootPath}");
+System.Console.WriteLine($"WebRoot Name: {builder.Environment.WebRootPath}");
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 string connectionString = builder.Configuration.GetConnectionString("FriendContext");
 
-builder.Services.Configure<DevelperOptions>(option=>{
+builder.Services.Configure<DevelperOptions>(option =>
+{
     builder.Configuration.GetSection("Developer").Bind(option);
 });
 
-builder.Services.AddDbContext<FriendContext>(options=>{
+builder.Services.AddDbContext<FriendContext>(options =>
+{
     options.UseSqlServer(connectionString);
 });
 
@@ -27,7 +44,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles (); // 使用預設 wwwroot 資料夾
+
+app.UseStaticFiles(new StaticFileOptions{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "StaticFilesLibrary")),
+    RequestPath = "/StaticFiles"
+});
 
 app.UseRouting();
 
